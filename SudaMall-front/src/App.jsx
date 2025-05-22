ed8658df8bff67d3bc3a15e323d59eb5b63a110a
Akram-Adam <akramadam050@gmail.com>
2025-05-22 19:02:39 +0200

    Initial Work Space

---
+++ SudaMall-front/src/App.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import './index.css'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-amber-500'>
+        <h1>akram</h1>  
+        </div>
+    </>
+  )
+}
+
+export default App
