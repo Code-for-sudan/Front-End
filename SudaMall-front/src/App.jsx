c44062fd3774b369e94535c6bade4a5ff71d2c32
Akram-Adam <akramadam050@gmail.com>
2025-06-24 11:04:06 +0200

    refactor: setup correct Router context and SPA navigation handling
    - Moved <BrowserRouter> from App.jsx to main.jsx to prevent nested Router error
    - Enabled useNavigate() inside App.jsx by wrapping App in <BrowserRouter> at the root
    - Removed duplicated <Router> from App.jsx to follow React Router rules
    - Integrated QueryClientProvider and BrowserRouter together properly in main.jsx
    - Fixed navigation redirection logic using navigateService and useNavigate inside useEffect

--- SudaMall-front/src/App.jsx
+++ SudaMall-front/src/App.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
+
 import './index.css'
 import {BrowserRouter as Router} from 'react-router-dom';
 import PublicRoutes from './routes/PublicRoutes';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { setNavigate } from './hooks/navigateService';
-const queryClient = new QueryClient();
+import { setNavigate } from '../src/hooks/navigateService';
+
 
 const App = () => {
     const navigate = useNavigate();
@@ -17,11 +14,9 @@ const App = () => {
     setNavigate(navigate);
   }, [navigate]);
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
+  
         <PublicRoutes />
-      </Router>
-    </QueryClientProvider>
+
   )
 }
 
