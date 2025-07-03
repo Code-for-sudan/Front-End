023a3bd5feef2691d6d6d42ae476041bc69a2397
MohamedAlamen123 <alamenmohamed536@gmail.com>
2025-07-03 21:42:33 +0200

    starting profile

--- SudaMall-front/src/data/user.js
+++ SudaMall-front/src/data/user.js
@@ -2,8 +2,18 @@ import { profile_pic } from "../assets/demo_data";
 
 export const userData = {
     id: 12,
-    email: "user@example.com",
     name: "محمد الامين",
+    email: "user@example.com",
+    phone: "+249912345678",
+    location: "الخرطوم، شارع الحرية",
     role: "تاجر",
     profile_pic: profile_pic,
+    store_info: {
+        store_id: 2,
+        store_name: "احمد ستور",
+        store_description: "جميع انواع الاحذية الرجالية والنسائية",
+        store_location: "الخرطوم، شارع الحرية",
+        activity_type: "أحذية",
+        store_license: "pic",
+    }
 }
