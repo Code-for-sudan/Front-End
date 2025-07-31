7d2872add9ec32f0c40449448ca55d964a7387ea
Alnyle <corgoo7z49h@gmail.com>
2025-07-31 18:51:03 +0300

    add functionality to back button

--- SudaMall-front/src/components/customer/customer-main/NavBar.jsx
+++ SudaMall-front/src/components/customer/customer-main/NavBar.jsx
@@ -3,11 +3,27 @@ import { FaArrowLeft } from "react-icons/fa";
 import { MdFavoriteBorder } from "react-icons/md";
 import { MdFavorite } from "react-icons/md";
 import { ArrowCircleRightR } from '../../../assets';
+import { useNavigate } from 'react-router-dom';
 
 function NavBar({ title, isFavorite = false, handleFavoriteToggle }) {
+
+
+  const navigate = useNavigate()
+
+  // navigate to previous page
+
+  const handleBack = () => {
+    navigate(-1)
+  }
+
   return (
     <div className="pt-8 px-6 flex items-center justify-between">
-        <img src={ArrowCircleRightR}  className='text-3xl size-8 text-black cursor-pointer' />
+        <img 
+          onClick={() => handleBack()}
+          src={ArrowCircleRightR}  
+          className='text-3xl size-8 text-black cursor-pointer'
+
+        />
         <p className='text-xl text-black'>{title}</p>
         <div 
           onClick={() => handleFavoriteToggle(!isFavorite)}
