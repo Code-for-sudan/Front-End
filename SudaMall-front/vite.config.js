ad68f3b5f56018b498f7e088a1244fcc036db3ec
ProAbdallaMI <proabdallami@gmail.com>
2025-08-13 23:46:12 +0300

    https cabability

--- SudaMall-front/vite.config.js
+++ SudaMall-front/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' 
+import mkcert from 'vite-plugin-mkcert'
 export default defineConfig({
   plugins: [
     react(),  
-    tailwindcss()
-      
+    tailwindcss(),
+    mkcert()
   ],
    server: {
     host: '0.0.0.0', // allows access via LAN
